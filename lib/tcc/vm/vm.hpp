22b6badc63745a5fd93861cf4549da8b10d29f69
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-11 15:30:15 +0100

    Add bytecodes to trace output

--- lib/tcc/vm/vm.hpp
+++ lib/tcc/vm/vm.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 
+#include <iostream>
+#include <string_view>
 #include <vector>
 
 namespace tcc
@@ -26,6 +28,30 @@ enum ByteCode : int64_t
     HALT   = 16,
 };
 
+std::ostream& operator<<(std::ostream& out, ByteCode byteCode)
+{
+    switch (byteCode)
+    {
+        case ByteCode::IADD: return out << "IADD";
+        case ByteCode::ISUB: return out << "ISUB";
+        case ByteCode::IMUL: return out << "IMUL";
+        case ByteCode::ILT: return out << "ILT";
+        case ByteCode::IEQ: return out << "IEQ";
+        case ByteCode::BR: return out << "BR";
+        case ByteCode::BRT: return out << "BRT";
+        case ByteCode::BRF: return out << "BRF";
+        case ByteCode::ICONST: return out << "ICONST";
+        case ByteCode::LOAD: return out << "LOAD";
+        case ByteCode::GLOAD: return out << "GLOAD";
+        case ByteCode::STORE: return out << "STORE";
+        case ByteCode::GSTORE: return out << "GSTORE";
+        case ByteCode::PRINT: return out << "PRINT";
+        case ByteCode::POP: return out << "POP";
+        case ByteCode::HALT: return out << "HALT";
+    }
+    return out;
+}
+
 class VirtualMachine
 {
 public:
@@ -40,7 +66,11 @@ public:
         while (static_cast<size_t>(m_instructionPointer) < m_code.size())
         {
             auto const opcode = m_code.at(m_instructionPointer);  // fetch instructions
-            if (m_isDebug) std::printf("%04lld: %lld\n", m_instructionPointer, opcode);
+            if (m_isDebug)
+            {
+                std::printf("%04lld: ", m_instructionPointer);
+                std::cout << ByteCode{opcode} << '\n';
+            }
 
             m_instructionPointer++;  // advance instruction pointer
 

17df94d33c426214831cecdab7a4b53d426512e4
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-13 05:08:42 +0100

    Split vm into more source files

---
+++ lib/tcc/vm/byte_code.cpp
@@ -0,0 +1,31 @@
+#include "tcc/vm/byte_code.hpp"
+
+namespace tcc
+{
+
+std::ostream& operator<<(std::ostream& out, ByteCode byteCode)
+{
+    switch (byteCode)
+    {
+        case ByteCode::IADD: return out << "IADD";
+        case ByteCode::ISUB: return out << "ISUB";
+        case ByteCode::IMUL: return out << "IMUL";
+        case ByteCode::ILT: return out << "ILT";
+        case ByteCode::IEQ: return out << "IEQ";
+        case ByteCode::BR: return out << "BR";
+        case ByteCode::BRT: return out << "BRT";
+        case ByteCode::BRF: return out << "BRF";
+        case ByteCode::ICONST: return out << "ICONST";
+        case ByteCode::LOAD: return out << "LOAD";
+        case ByteCode::GLOAD: return out << "GLOAD";
+        case ByteCode::STORE: return out << "STORE";
+        case ByteCode::GSTORE: return out << "GSTORE";
+        case ByteCode::PRINT: return out << "PRINT";
+        case ByteCode::POP: return out << "POP";
+        case ByteCode::CALL: return out << "CALL";
+        case ByteCode::RET: return out << "RET";
+        case ByteCode::HALT: return out << "HALT";
+    }
+    return out;
+}
+}  // namespace tcc
---
+++ lib/tcc/vm/vm.cpp
@@ -0,0 +1,212 @@
+#include "tcc/vm/vm.hpp"
+
+namespace tcc
+{
+VirtualMachine::VirtualMachine(std::vector<int64_t> code, int64_t const main, int64_t const dataSize,
+                               int64_t const stackSize)
+    : m_instructionPointer(main), m_code(std::move(code)), m_data(dataSize), m_stack(stackSize)
+{
+}
+
+void VirtualMachine::Cpu()
+{
+
+    while (static_cast<size_t>(m_instructionPointer) < m_code.size())
+    {
+        auto const opcode = m_code.at(m_instructionPointer);  // fetch instructions
+        if (m_shouldTrace)
+        {
+            disassemble(opcode);
+        }
+
+        m_instructionPointer++;  // advance instruction pointer
+
+        switch (opcode)
+        {
+            case ByteCode::IADD:
+            {
+                auto const b              = m_stack[m_stackPointer--];  // 2nd operand
+                auto const a              = m_stack[m_stackPointer--];  // 1st operand
+                m_stack[++m_stackPointer] = a + b;                      // push result
+                break;
+            }
+
+            case ByteCode::ISUB:
+            {
+                auto const b              = m_stack[m_stackPointer--];  // 2nd operand
+                auto const a              = m_stack[m_stackPointer--];  // 1st operand
+                m_stack[++m_stackPointer] = a - b;                      // push result
+                break;
+            }
+
+            case ByteCode::IMUL:
+            {
+                auto const b              = m_stack[m_stackPointer--];  // 2nd operand
+                auto const a              = m_stack[m_stackPointer--];  // 1st operand
+                m_stack[++m_stackPointer] = a * b;                      // push result
+                break;
+            }
+
+            case ByteCode::ILT:
+            {
+                auto const b              = m_stack[m_stackPointer--];  // 2nd operand
+                auto const a              = m_stack[m_stackPointer--];  // 1st operand
+                m_stack[++m_stackPointer] = a < b ? 1 : 0;              // push result
+                break;
+            }
+
+            case ByteCode::IEQ:
+            {
+                auto const b              = m_stack[m_stackPointer--];  // 2nd operand
+                auto const a              = m_stack[m_stackPointer--];  // 1st operand
+                m_stack[++m_stackPointer] = a == b ? 1 : 0;             // push result
+                break;
+            }
+
+            case ByteCode::BR:
+            {
+                m_instructionPointer = m_code[m_instructionPointer++];
+                break;
+            }
+
+            case ByteCode::BRT:
+            {
+                auto const addr = m_code[m_instructionPointer++];
+                if (m_stack[m_stackPointer--]) m_instructionPointer = addr;
+                break;
+            }
+
+            case ByteCode::BRF:
+            {
+                auto const addr = m_code[m_instructionPointer++];
+                if (!m_stack[m_stackPointer--]) m_instructionPointer = addr;
+                break;
+            }
+
+            case ByteCode::ICONST:
+            {
+                auto const val = m_code.at(m_instructionPointer);  // read operand from code
+                m_instructionPointer++;
+                m_stackPointer++;
+                m_stack[m_stackPointer] = val;  // push to stack
+                break;
+            }
+
+            case ByteCode::LOAD:
+            {
+                auto const offset         = m_code[m_instructionPointer++];
+                m_stack[++m_stackPointer] = m_stack[m_framePointer + offset];
+                break;
+            }
+
+            case ByteCode::GLOAD:
+            {
+                auto const addr = m_code.at(m_instructionPointer);
+                m_instructionPointer++;
+                auto const val = m_data.at(addr);
+                m_stackPointer++;
+                m_stack.at(m_stackPointer) = val;
+                break;
+            }
+
+            case ByteCode::STORE:
+            {
+                auto const offset                = m_code[m_instructionPointer++];
+                m_stack[m_framePointer + offset] = m_stack[m_stackPointer--];
+                break;
+            }
+
+            case ByteCode::GSTORE:
+            {
+                auto const val = m_stack.at(m_stackPointer);
+                m_stackPointer--;
+                auto const addr = m_code.at(m_instructionPointer);
+                m_instructionPointer++;
+                m_data.at(addr) = val;
+                break;
+            }
+
+            case ByteCode::PRINT:
+            {
+                auto const val = m_stack.at(m_stackPointer);
+                m_stackPointer--;
+                std::printf("%lld\n", val);
+                break;
+            }
+
+            case ByteCode::POP:
+            {
+                --m_stackPointer;
+                break;
+            }
+
+            case ByteCode::CALL:
+            {
+                // expects all args on stack
+                auto const addr           = m_code[m_instructionPointer++];  // addr of function
+                auto const numArgs        = m_code[m_instructionPointer++];  // how many args got pushed
+                m_stack[++m_stackPointer] = numArgs;                         // save num args
+                m_stack[++m_stackPointer] = m_framePointer;                  // save frame pointer
+                m_stack[++m_stackPointer] = m_instructionPointer;            // save instruction pointer
+                m_framePointer            = m_stackPointer;                  // fp points to return addr on stack
+                m_instructionPointer      = addr;                            // jump to function
+                break;
+            }
+
+            case ByteCode::RET:
+            {
+                auto const returnVal = m_stack.at(m_stackPointer--);  // pop return value
+                m_stackPointer       = m_framePointer;                // jump over locals to frame pointer
+                m_instructionPointer = m_stack.at(m_stackPointer--);  // pop return addr
+                m_framePointer       = m_stack.at(m_stackPointer--);  // restore frame pointer
+                auto const numArgs   = m_stack.at(m_stackPointer--);  // how many args to throw away
+                m_stackPointer -= numArgs;                            // pop args
+                m_stack.at(++m_stackPointer) = returnVal;             // leave result on stack
+                break;
+            }
+
+            case ByteCode::HALT: return;
+        }
+    }
+}
+
+void VirtualMachine::EnableTracing(bool const shouldTrace) { m_shouldTrace = shouldTrace; }
+
+void VirtualMachine::disassemble(int64_t const opcode)
+{
+    auto const instruction = Instructions[opcode];
+    std::printf("%04lld: ", m_instructionPointer);
+    std::stringstream byteCodeStr{};
+    byteCodeStr << ByteCode{opcode};
+    std::printf("%s", byteCodeStr.str().c_str());
+    if (instruction.numberOfOperands == 1) std::printf(" %lld", m_code.at(m_instructionPointer + 1));
+    if (instruction.numberOfOperands == 2)
+    {
+        auto const firstOperand  = m_code.at(m_instructionPointer + 1);
+        auto const secondOperand = m_code.at(m_instructionPointer + 2);
+        std::printf(" %lld, %lld", firstOperand, secondOperand);
+    }
+
+    printGlobalMemory();
+    std::printf("\t STACK_PTR: %2lld", m_stackPointer);
+    printStack();
+    std::puts("");
+}
+void VirtualMachine::printStack()
+{
+    std::printf("\t STACK: [ ");
+    for (auto i = 0; i <= m_stackPointer; i++)
+    {
+        auto const var = m_stack.at(i);
+        std::printf("%lld ", var);
+    }
+    std::printf("]");
+}
+void VirtualMachine::printGlobalMemory()
+{
+    std::printf("\t\t GLOBALS: [ ");
+    for (auto const& global : m_data) std::printf("%04lld ", global);
+    std::printf("]");
+}
+
+}  // namespace tcc

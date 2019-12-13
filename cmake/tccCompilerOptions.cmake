f65af8861caf249ea80cfcf48b1df992aa5e15b4
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-13 07:34:13 +0100

    Fix cmake

--- cmake/tccCompilerOptions.cmake
+++
@@ -1,5 +0,0 @@
-add_library(compiler_options INTERFACE)
-add_library(tcc::CompilerOptions ALIAS compiler_options)
-
-# Language standard
-target_compile_features(compiler_options INTERFACE cxx_std_17)

b6bd91902fc68d688fa16b4c77fde12b05afe14b
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-13 06:23:55 +0100

    Cleanup main

--- src/main.cpp
+++ src/main.cpp
@@ -32,24 +32,6 @@ int main(int const argc, char const** const argv)
 
     using namespace tcc;
 
-    auto const instructions = std::vector<int64_t>{
-        ByteCode::ICONST, 99,  // 0
-        ByteCode::GSTORE, 0,   // 2
-        ByteCode::GLOAD,  0,   // 4
-        ByteCode::ICONST, 2,   // 6
-        ByteCode::ILT,         // 8
-        ByteCode::BRT,    13,  // 9
-        ByteCode::ICONST, 42,  // 11
-        ByteCode::ICONST, 54,  // 13
-        ByteCode::PRINT,       // 15
-        ByteCode::HALT,        // 16
-
-        ByteCode::ICONST, 143,  // 15
-        ByteCode::PRINT,        // 17
-        ByteCode::HALT,         // 18
-
-    };
-
     auto const factorial = std::vector<int64_t>{
         // .def fact: args=1, locals=0
         // if n < 2 return 1
@@ -74,25 +56,11 @@ int main(int const argc, char const** const argv)
         ByteCode::EXIT,         // 27
     };
 
-    tcc::BinaryExpression expression = {
-        new tcc::ConstantExpression(10),
-        new tcc::BinaryExpression(            //
-            new tcc::ConstantExpression(5),   //
-            new tcc::ConstantExpression(5),   //
-            tcc::BinaryExpression::Type::Add  //
-            ),
-        tcc::BinaryExpression::Type::Add  //
-    };
+    auto const entryPoint       = 22;
+    auto const globalMemorySize = 0;
+    auto const stackSize        = 200;
 
-    auto assembly = expression.GetAssembly();
-    assembly.push_back(tcc::ByteCode::PRINT);
-    assembly.push_back(tcc::ByteCode::HALT);
-
-    auto const stackSize = 200;
-
-    // auto vm = VirtualMachine(factorial, 22, 0, stackSize);
-    // auto vm = VirtualMachine(instructions, 0, 4, stackSize);
-    auto vm             = VirtualMachine(assembly, 0, 0, stackSize);
+    auto vm             = VirtualMachine(factorial, entryPoint, globalMemorySize, stackSize);
     auto const exitCode = vm.Cpu();
 
     return EXIT_SUCCESS;

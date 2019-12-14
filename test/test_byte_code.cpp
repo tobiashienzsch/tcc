c5aff1153c678fab5cf5cb085fb43037f19c202b
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-14 03:36:00 +0100

    Add test for bytecode ostream

---
+++ test/test_byte_code.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file test_byte_code.cpp
+ * @copyright Copyright 2019 Modern Circuits. All rights reserved.
+ */
+#include "catch2/catch.hpp"
+
+#include <sstream>
+
+#include "tcc/vm/byte_code.hpp"
+
+TEST_CASE("vm: ByteCode ostream", "[vm]")
+{
+    auto const input = {
+        tcc::ByteCode::ICONST,  //
+        tcc::ByteCode::IADD,    //
+        tcc::ByteCode::ISUB,    //
+        tcc::ByteCode::IMUL,    //
+        tcc::ByteCode::ILT,     //
+        tcc::ByteCode::IEQ,     //
+        tcc::ByteCode::PRINT,   //
+        tcc::ByteCode::STORE,   //
+        tcc::ByteCode::LOAD,    //
+        tcc::ByteCode::GSTORE,  //
+        tcc::ByteCode::GLOAD,   //
+        tcc::ByteCode::BR,      //
+        tcc::ByteCode::BRT,     //
+        tcc::ByteCode::BRF,     //
+        tcc::ByteCode::POP,     //
+        tcc::ByteCode::CALL,    //
+        tcc::ByteCode::RET,     //
+        tcc::ByteCode::EXIT,    //
+        tcc::ByteCode::HALT     //
+    };
+
+    auto stream = std::stringstream{};
+    for (auto const& x : input) stream << x;
+    auto const str = stream.str();
+
+    REQUIRE(str.compare("ICONSTIADDISUBIMULILTIEQPRINTSTORELOADGSTOREGLOADBRBRTBRFPOPCALLRETEXITHALT") == 0);
+}

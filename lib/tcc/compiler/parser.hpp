3477af402c76772e21824507e6616453e8ea1487
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-15 22:35:14 +0100

    Cleanup parser

--- lib/tcc/compiler/parser.hpp
+++ lib/tcc/compiler/parser.hpp
@@ -10,65 +10,6 @@
 
 namespace tcc
 {
-enum class ExpressionType
-{
-    Number,
-    Binary,
-};
-
-class SyntaxNode
-{
-public:
-    virtual ~SyntaxNode()                  = default;
-    virtual ExpressionType GetType() const = 0;
-    virtual void Print() const             = 0;
-};
-
-class ExpressionSyntax : public SyntaxNode
-{
-};
-class NumberExpressionSyntax : public ExpressionSyntax
-{
-public:
-    NumberExpressionSyntax(SyntaxToken token) : m_token(token) {}
-    ~NumberExpressionSyntax() override = default;
-    ExpressionType GetType() const override { return ExpressionType::Number; }
-    void Print() const override { std::cout << "NumberExpression\n"; }
-
-private:
-    SyntaxToken m_token;
-};
-
-class BinaryExpressionSyntax : public ExpressionSyntax
-{
-public:
-    BinaryExpressionSyntax(std::shared_ptr<ExpressionSyntax const> left, std::shared_ptr<ExpressionSyntax const> right,
-                           SyntaxToken const token)
-        : m_left(left), m_right(right), m_operatorToken(token)
-    {
-    }
-
-    ~BinaryExpressionSyntax() override = default;
-
-    ExpressionType GetType() const override { return ExpressionType::Binary; }
-
-    void Print() const override
-    {
-        std::cout << "Left: \n";
-        m_left->Print();
-
-        std::cout << m_operatorToken << '\n';
-
-        std::cout << "Left: \n";
-        m_left->Print();
-    }
-
-private:
-    std::shared_ptr<ExpressionSyntax const> m_left;
-    std::shared_ptr<ExpressionSyntax const> m_right;
-    SyntaxToken const m_operatorToken;
-};
-
 class Parser
 {
 public:
@@ -89,32 +30,32 @@ public:
         } while (type != SyntaxToken::Type::EndOfFile);
     }
 
-    std::shared_ptr<ExpressionSyntax> Parse()
-    {
-        auto const current = currentToken();
-        auto left          = parsePrimaryExpression();
-        left->Print();
-
-        while (current.type == SyntaxToken::Type::Plus || current.type == SyntaxToken::Type::Minus)
-        {
-            auto const operatorToken = nextToken();
-            auto const right         = parsePrimaryExpression();
-            right->Print();
-
-            left = std::make_shared<BinaryExpressionSyntax>(left, right, operatorToken);
-        }
-
-        return left;
-    }
-
     tcc::SyntaxTokenList& GetTokens() { return m_tokens; }
 
+    // std::shared_ptr<ExpressionSyntax> Parse()
+    // {
+    //     auto const current = currentToken();
+    //     auto left          = parsePrimaryExpression();
+    //     left->Print();
+
+    //     while (current.type == SyntaxToken::Type::Plus || current.type == SyntaxToken::Type::Minus)
+    //     {
+    //         auto const operatorToken = nextToken();
+    //         auto const right         = parsePrimaryExpression();
+    //         right->Print();
+
+    //         left = std::make_shared<BinaryExpressionSyntax>(left, right, operatorToken);
+    //     }
+
+    //     return left;
+    // }
+
 private:
-    std::shared_ptr<ExpressionSyntax> parsePrimaryExpression()
-    {
-        auto const numberToken = matchTokenType(SyntaxToken::Type::Number);
-        return std::make_shared<NumberExpressionSyntax>(numberToken);
-    }
+    // std::shared_ptr<ExpressionSyntax> parsePrimaryExpression()
+    // {
+    //     auto const numberToken = matchTokenType(SyntaxToken::Type::Number);
+    //     return std::make_shared<NumberExpressionSyntax>(numberToken);
+    // }
 
     SyntaxToken matchTokenType(SyntaxToken::Type type)
     {
---
+++ test/test_parser.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file test_parser.cpp
+ * @copyright Copyright 2019 Modern Circuits. All rights reserved.
+ */
+#include "catch2/catch.hpp"
+
+#include "tcc/compiler/parser.hpp"
+
+TEST_CASE("parser: Simple", "[parser]")
+{
+    auto parser = tcc::Parser{"auto main(){}"};
+    // auto token  = parser.Parse();
+    // REQUIRE(token.type == tcc::SyntaxToken::Type::EndOfFile);
+}

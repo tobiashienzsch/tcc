d3821bfd4cefa3f98f3273d8a3b463e02c5feb8a
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-17 13:28:34 +0100

    Extend spirit x3 parser example

--- playground/main_spirit_parser.cpp
+++ playground/main_spirit_parser.cpp
@@ -94,232 +94,90 @@
 
 #include <boost/config/warning_disable.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
+#include <boost/fusion/include/std_tuple.hpp>
 #include <boost/spirit/home/x3.hpp>
 #include <boost/spirit/home/x3/support/ast/variant.hpp>
 
 #include <iostream>
 #include <list>
+#include <map>
 #include <numeric>
 #include <string>
 
 namespace x3 = boost::spirit::x3;
 
-namespace client
+int main()
 {
-namespace ast
-{
-///////////////////////////////////////////////////////////////////////////
-//  The AST
-///////////////////////////////////////////////////////////////////////////
-struct nil
-{
-};
-struct signed_;
-struct program;
 
-struct operand : x3::variant<nil, unsigned int, x3::forward_ast<signed_>, x3::forward_ast<program>>
-{
-    using base_type::base_type;
-    using base_type::operator=;
-};
+    auto const input = std::string(R"(
+        package main;
 
-struct signed_
-{
-    char sign;
-    operand operand_;
-};
+        auto some_var = expression;
 
-struct operation
-{
-    char operator_;
-    operand operand_;
-};
+        auto foo_bar(Tbar, _test) -> void {}
+        auto no_returnType() {}
 
-struct program
-{
-    operand first;
-    std::list<operation> rest;
-};
-}  // namespace ast
-}  // namespace client
+        auto main() -> int {}
+    )");
 
-BOOST_FUSION_ADAPT_STRUCT(client::ast::signed_, sign, operand_)
+    auto iter     = input.begin();
+    auto iter_end = input.end();
 
-BOOST_FUSION_ADAPT_STRUCT(client::ast::operation, operator_, operand_)
+    auto const identifier = x3::rule<class identifier, std::string>()  //
+        = x3::raw[                                                     //
+            x3::lexeme[                                                //
+                (x3::alpha | '_')                                      //
+                >> *(x3::alnum | '_')                                  //
+    ]                                                                  //
+    ];                                                                 //
 
-BOOST_FUSION_ADAPT_STRUCT(client::ast::program, first, rest)
+    auto const functionDefinition = x3::rule<class functionDefinition, std::vector<std::string>>()
+        = x3::lit("auto")                    //
+          >> identifier                      //
+          >> x3::lit("(")                    //
+          >> *(identifier % ',')             //
+          >> x3::lit(")")                    //
+          >> -(x3::lit("->") >> identifier)  //
+          >> x3::lit("{}")                   //
+        ;                                    //
 
-namespace client
-{
-namespace ast
-{
-///////////////////////////////////////////////////////////////////////////
-//  The AST Printer
-///////////////////////////////////////////////////////////////////////////
-struct printer
-{
-    typedef void result_type;
+    auto const variableDefinition = x3::rule<class variableDefinition, std::vector<std::string>>()
+        = x3::lit("auto")           //
+          >> identifier             //
+          >> x3::lit("=")           //
+          >> x3::lit("expression")  //
+          >> x3::lit(";")           //
+        ;                           //
 
-    void operator()(nil) const {}
-    void operator()(unsigned int n) const { std::cout << n; }
+    auto const packageDefinition
+        = x3::rule<class packageDefinition, std::tuple<std::string, std::vector<std::vector<std::string>>>>()
+        = x3::lit("package")                             //
+          >> identifier                                  //
+          >> x3::lit(";")                                //
+          >> +(variableDefinition | functionDefinition)  //
+        ;                                                //
 
-    void operator()(operation const& x) const
+    std::tuple<std::string, std::vector<std::vector<std::string>>> result;
+
+    phrase_parse(iter,      //
+                 iter_end,  //
+
+                 // START GRAMMER
+
+                 packageDefinition,  //
+
+                 // END GRAMMER
+
+                 x3::space,  //
+                 result      //
+    );
+
+    std::cout << "Package: " << std::get<0>(result) << '\n';
+    for (auto const& x : std::get<1>(result))
     {
-        boost::apply_visitor(*this, x.operand_);
-        switch (x.operator_)
+        for (auto const& y : x)
         {
-            case '+': std::cout << " add"; break;
-            case '-': std::cout << " subt"; break;
-            case '*': std::cout << " mult"; break;
-            case '/': std::cout << " div"; break;
+            std::cout << y << '\n';
         }
     }
-
-    void operator()(signed_ const& x) const
-    {
-        boost::apply_visitor(*this, x.operand_);
-        switch (x.sign)
-        {
-            case '-': std::cout << " neg"; break;
-            case '+': std::cout << " pos"; break;
-        }
-    }
-
-    void operator()(program const& x) const
-    {
-        boost::apply_visitor(*this, x.first);
-        for (operation const& oper : x.rest)
-        {
-            std::cout << ' ';
-            (*this)(oper);
-        }
-    }
-};
-
-///////////////////////////////////////////////////////////////////////////
-//  The AST evaluator
-///////////////////////////////////////////////////////////////////////////
-struct eval
-{
-    typedef int result_type;
-
-    int operator()(nil) const
-    {
-        BOOST_ASSERT(0);
-        return 0;
-    }
-    int operator()(unsigned int n) const { return n; }
-
-    int operator()(int lhs, operation const& x) const
-    {
-        int rhs = boost::apply_visitor(*this, x.operand_);
-        switch (x.operator_)
-        {
-            case '+': return lhs + rhs;
-            case '-': return lhs - rhs;
-            case '*': return lhs * rhs;
-            case '/': return lhs / rhs;
-        }
-        BOOST_ASSERT(0);
-        return 0;
-    }
-
-    int operator()(signed_ const& x) const
-    {
-        int rhs = boost::apply_visitor(*this, x.operand_);
-        switch (x.sign)
-        {
-            case '-': return -rhs;
-            case '+': return +rhs;
-        }
-        BOOST_ASSERT(0);
-        return 0;
-    }
-
-    int operator()(program const& x) const
-    {
-        return std::accumulate(x.rest.begin(), x.rest.end(), boost::apply_visitor(*this, x.first), *this);
-    }
-};
-}  // namespace ast
-}  // namespace client
-
-namespace client
-{
-
-namespace calculator_grammar
-{
-using x3::char_;
-using x3::uint_;
-
-x3::rule<class expression, ast::program> const expression("expression");
-x3::rule<class term, ast::program> const term("term");
-x3::rule<class factor, ast::operand> const factor("factor");
-
-auto const expression_def =        //
-    term                           //
-    >> *((char_('+') >> term)      //
-         | (char_('-') >> term));  //
-
-auto const term_def =                //
-    factor                           //
-    >> *((char_('*') >> factor)      //
-         | (char_('/') >> factor));  //
-
-auto const factor_def =         //
-    uint_                       //
-    | '(' >> expression >> ')'  //
-    | (char_('-') >> factor)    //
-    | (char_('+') >> factor);   //
-
-BOOST_SPIRIT_DEFINE(  //
-    expression,       //
-    term,             //
-    factor            //
-);
-
-auto calculator = expression;
-}  // namespace calculator_grammar
-
-using calculator_grammar::calculator;
-
-}  // namespace client
-
-int main(int argc, char** argv)
-{
-    std::string str = "(1+2)*3+5";
-    if (argc == 2)
-    {
-        str = std::string(argv[1]);
-    }
-
-    using ast_program = client::ast::program;
-    using ast_print   = client::ast::printer;
-    using ast_eval    = client::ast::eval;
-
-    auto& calc = client::calculator;  // Our grammar
-    ast_program program;              // Our program (AST)
-    ast_print print;                  // Prints the program
-    ast_eval eval;                    // Evaluates the program
-
-    auto iter = std::begin(str);
-    auto end  = std::end(str);
-    boost::spirit::x3::ascii::space_type space;
-    bool r = phrase_parse(iter, end, calc, space, program);
-
-    if (r && iter == end)
-    {
-        std::cout << "Parsing succeeded\n";
-        print(program);
-        std::cout << "\nResult: " << eval(program) << std::endl;
-    }
-    else
-    {
-        std::string rest(iter, end);
-        std::cout << "Parsing failed\n";
-        std::cout << "stopped at: \"" << rest << "\"\n";
-    }
-
-    std::cout << "Bye... :-) \n\n";
-    return 0;
 }

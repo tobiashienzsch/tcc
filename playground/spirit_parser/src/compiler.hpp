85b64a9c622b81739751d45cc5d29796b5ab3465
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-19 04:50:56 +0100

    Create statement list in ir builder

--- playground/spirit_parser/src/compiler.hpp
+++ playground/spirit_parser/src/compiler.hpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <map>
+#include <optional>
 #include <string>
 #include <variant>
 #include <vector>
@@ -19,80 +20,104 @@ namespace code_gen
 
 struct ir_builder
 {
-    struct temp
+    struct ir_temp
     {
         std::string name;
     };
 
+    struct ir_statement
+    {
+        byte_code type;
+        std::string destination;
+        std::variant<int, ir_temp> first;
+        std::optional<std::variant<int, ir_temp>> second;
+
+        friend std::ostream& operator<<(std::ostream& out, ir_statement const& data)
+        {
+            auto firstStr = std::string {};
+            if (std::holds_alternative<int>(data.first)) firstStr = std::to_string(std::get<int>(data.first));
+            if (std::holds_alternative<ir_temp>(data.first))
+                firstStr = fmt::format("%{}", std::get<ir_temp>(data.first).name);
+
+            auto secondStr = std::string {};
+            if (data.second.has_value())
+            {
+                auto const second = data.second.value();
+                if (std::holds_alternative<int>(second)) secondStr = std::to_string(std::get<int>(second));
+                if (std::holds_alternative<ir_temp>(second))
+                    secondStr = fmt::format("%{}", std::get<ir_temp>(second).name);
+            }
+
+            std::stringstream opCodeStr;
+            opCodeStr << static_cast<byte_code>(data.type);
+            auto const str
+                = fmt::format("{0}\t:=\t{1}\t{2}\t{3}", data.destination, firstStr, opCodeStr.str(), secondStr);
+
+            return out << str;
+        }
+    };
+
     void PushToStack(int x) { m_stack.push_back(x); }
+
+    auto PopFromStack()
+    {
+        auto result = m_stack.back();
+        m_stack.pop_back();
+        return result;
+    }
 
     void CreateBinaryOperation(int op)
     {
-        auto second = m_stack.back();
-        m_stack.pop_back();
+        auto const second  = PopFromStack();
+        auto const first   = PopFromStack();
+        auto const tmpName = CreateTemporaryOnStack();
 
-        auto secondStr = std::string {};
-        if (std::holds_alternative<int>(second)) secondStr = std::to_string(std::get<int>(second));
-        if (std::holds_alternative<temp>(second)) secondStr = fmt::format("%{}", std::get<temp>(second).name);
-
-        auto first = m_stack.back();
-        m_stack.pop_back();
-
-        auto firstStr = std::string {};
-        if (std::holds_alternative<int>(first)) firstStr = std::to_string(std::get<int>(first));
-        if (std::holds_alternative<temp>(first)) firstStr = fmt::format("%{}", std::get<temp>(first).name);
-
-        auto tmpName = std::string {"t"}.append(std::to_string(m_varCounter++));
-        auto tmp     = temp {tmpName};
-        m_stack.push_back(tmp);
-
-        std::stringstream opCodeStr;
-        opCodeStr << static_cast<byte_code>(op);
-        fmt::print("{0}\t:=\t{1}\t{2}\t{3}\n", tmp.name, firstStr, opCodeStr.str(), secondStr);
+        m_statements.push_back(ir_statement {static_cast<byte_code>(op), tmpName, first, second});
     }
 
     void CreateUnaryOperation(int op)
     {
-        auto first = m_stack.back();
-        m_stack.pop_back();
-
-        auto firstStr = std::string {};
-        if (std::holds_alternative<int>(first)) firstStr = std::to_string(std::get<int>(first));
-        if (std::holds_alternative<temp>(first)) firstStr = fmt::format("%{}", std::get<temp>(first).name);
-
-        auto tmpName = std::string {"t"}.append(std::to_string(m_varCounter++));
-        auto tmp     = temp {tmpName};
-        m_stack.push_back(tmp);
-
-        auto opCodeStr = std::stringstream {};
-        opCodeStr << static_cast<byte_code>(op);
-        fmt::print("{0}\t:=\t{1}\t{2}\n", tmp.name, firstStr, opCodeStr.str());
+        auto const first   = PopFromStack();
+        auto const tmpName = CreateTemporaryOnStack();
+        m_statements.push_back(ir_statement {static_cast<byte_code>(op), tmpName, first});
     }
 
     void CreateStoreOperation(std::string name)
     {
-        auto first = m_stack.back();
-        m_stack.pop_back();
-        auto firstStr = std::string {};
-        if (std::holds_alternative<int>(first)) firstStr = std::to_string(std::get<int>(first));
-        if (std::holds_alternative<temp>(first)) firstStr = fmt::format("%{}", std::get<temp>(first).name);
+        auto first = PopFromStack();
 
-        fmt::print("{0}\t:=\t{1}\tstore\n", name, firstStr);
+        m_statements.push_back(ir_statement {op_store, name, first});
     }
 
     void CreateLoadOperation(std::string name)
     {
-        auto tmpName = std::string {"t"}.append(std::to_string(m_varCounter++));
-        auto tmp     = temp {tmpName};
-        m_stack.push_back(tmp);
+        auto const tmpName = CreateTemporaryOnStack();
+        m_statements.push_back(ir_statement {op_load, tmpName, ir_temp {name}});
+    }
 
-        fmt::print("{0}\t:=\t%{1}\tload\n", tmpName, name);
+    void PrintStatementList() const
+    {
+        for (auto const& x : m_statements)
+        {
+            std::cout << x << '\n';
+        }
+        std::cout << "\n\n";
+    }
+
+    auto CreateTemporaryOnStack() -> std::string
+    {
+
+        auto tmpName = std::string {"t"}.append(std::to_string(m_varCounter++));
+        auto tmp     = ir_temp {tmpName};
+        m_stack.push_back(tmp);
+        return tmpName;
     }
 
 private:
     int m_varCounter = 0;
     std::map<std::string, int> variables;
-    std::vector<std::variant<int, temp>> m_stack;
+    std::vector<std::variant<int, ir_temp>> m_stack;
+    std::vector<ir_statement> m_statements;
 };
 
 ///////////////////////////////////////////////////////////////////////////

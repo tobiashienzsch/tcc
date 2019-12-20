56c234e72b2e63a0b28805d9d178958864940c86
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-20 06:41:27 +0100

    Set msvc warning level to 3

--- cmake/tccCompilerWarnings.cmake
+++ cmake/tccCompilerWarnings.cmake
@@ -2,7 +2,7 @@ add_library(compiler_warnings INTERFACE)
 add_library(tcc::CompilerWarnings ALIAS compiler_warnings)
 
 if(MSVC)
-  target_compile_options(compiler_warnings INTERFACE /W4 "/permissive-")
+  target_compile_options(compiler_warnings INTERFACE /W3 "/permissive-")
 else()
   # GCC & CLANG
   target_compile_options(

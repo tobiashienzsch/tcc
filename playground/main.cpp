4ec8dee48e0f4ab36f74f72b4afdaa648e15fa4a
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-21 22:43:29 +0100

    Add boost serialization

--- playground/main.cpp
+++ playground/main.cpp
@@ -1,9 +1,54 @@
-#include "tcc/core/binary_format.hpp"
-
 #include <cstdlib>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+#include <boost/serialization/vector.hpp>
+
+#include <fmt/format.h>
+
+struct BinaryProgram
+{
+    int64_t version{0};
+    char name[25]{0};
+    std::vector<int64_t> data;
+
+    template<class Archive>
+    void serialize(Archive& ar, const unsigned int /* file_version */)
+    {
+        ar& version;
+        ar& name;
+        ar& data;
+    }
+};
 
 int main(int, char**)
 {
-    tcc::BinaryFormat::Write("test.tobi", {1, 1, 1, 1, 1});
+
+    {
+        // make an archive
+        auto program = BinaryProgram{1, "test", {0, 1, 2, 3, 1, 4, 5, 2, 6, 4, 8, 2}};
+        std::ofstream ofs("test.tcb");
+        boost::archive::text_oarchive oa(ofs);
+        oa << program;
+    }
+
+    {
+        // open the archive
+        auto program = BinaryProgram{};
+        std::ifstream ifs("test.tcb");
+        boost::archive::text_iarchive ia(ifs);
+
+        // restore the schedule from the archive
+        ia >> program;
+
+        fmt::print("Name: {}\n", program.name);
+        for (auto x : program.data) fmt::print("{}\n", x);
+    }
+
     return EXIT_SUCCESS;
 }

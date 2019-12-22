d35c0dd0fe373f4adf76b56619887a1f8ad04711
Tobias Hienzsch <post@tobias-hienzsch.de>
2019-12-22 23:19:43 +0100

    core: Don't use temp dir in unit tests

--- lib/core/test/test_binary_format.cpp
+++ lib/core/test/test_binary_format.cpp
@@ -6,9 +6,6 @@
 
 #include "tcc/core/binary_format.hpp"
 
-#include <boost/filesystem.hpp>
-namespace fs = boost::filesystem;
-
 #include <sstream>
 
 TEST_CASE("core: BinaryFormat WriteReadStream", "[core]")
@@ -35,27 +32,23 @@ TEST_CASE("core: BinaryFormat WriteReadStream", "[core]")
 
 TEST_CASE("core: BinaryFormatWriteReadFile", "[core]")
 {
-    // temporary path
-    auto tempFile = fs::temp_directory_path();
-    tempFile += std::string("test_BinaryFormatWriteReadFile.tcb");
+    // temporary file
+    auto const tempFile = std::string("test_BinaryFormatWriteReadFile.tcb");
 
     // write to stream
     {
         auto const program = tcc::BinaryProgram{1, "test", 0, {1, 2, 3}};
-        tcc::BinaryFormat::WriteToFile(tempFile.string(), program);
+        tcc::BinaryFormat::WriteToFile(tempFile, program);
     }
 
     // read to stream
     {
         auto program = tcc::BinaryProgram{};
-        tcc::BinaryFormat::ReadFromFile(tempFile.string(), program);
+        tcc::BinaryFormat::ReadFromFile(tempFile, program);
 
         REQUIRE(program.version == 1);
         REQUIRE(program.name == std::string("test"));
         REQUIRE(program.entryPoint == 0);
         REQUIRE(program.data == std::vector<tcc::Integer>{1, 2, 3});
     }
-
-    // cleanup
-    fs::remove(tempFile);
 }

b35dc8cf624e8ad6f585326bcc9ddc1db432d20b
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-03-31 01:12:01 +0200

    neo: Parse package declaration

--- tcc-neo/main.cpp
+++ tcc-neo/main.cpp
@@ -1,19 +1,41 @@
-#include "tcc-neo/parser/identifier.hpp"
+#include "tcc-neo/parser/config.hpp"
+#include "tcc-neo/parser/error_handler.hpp"
+#include "tcc-neo/parser/package_def.hpp"
 
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
-    auto const source = std::string {"test"};
-    auto result       = std::string {};
-    tcc::parser::x3::phrase_parse(std::begin(source),       //
-                                  std::end(source),         //
-                                  tcc::parser::identifier,  //
-                                  tcc::parser::x3::space,   //
-                                  result                    //
-    );
+    auto const source = std::string {"package test;"};
+    
+    using tcc::parser::iterator_type;
+    iterator_type iter(source.begin());
+    iterator_type end(source.end());
+    std::printf("SOURCE:\n%s\n", source.c_str());
 
-    std::printf("%s\n", result.c_str());
+    // tcc::parser::PackageDeclaration_type ast;  // Our AST
+    std::string ast;  // Our AST
+
+    using boost::spirit::x3::with;
+    using tcc::parser::error_handler_type;
+    error_handler_type error_handler(iter, end, std::cerr);  // Our error handler
+
+    // Our parser
+    // we pass our error handler to the parser so we can access
+    // it later on in our on_error and on_sucess handlers
+    auto const parser = with<tcc::parser::error_handler_tag>(std::ref(error_handler))[tcc::PackageDeclaration()];
+    bool success      = tcc::parser::x3::phrase_parse(iter, end, parser, tcc::parser::x3::space, ast);
+
+    if (success && iter == end)
+    {
+        std::printf("Parse success: %s\n", ast.c_str());
+    }
+    else
+    {
+        std::printf("Parse failed!\n");
+    }
+
     return EXIT_SUCCESS;
 }
--- tcc-neo/tcc-neo/parser/package_def.hpp
+++ tcc-neo/tcc-neo/parser/package_def.hpp
@@ -5,7 +5,7 @@
 #include "tcc-neo/parser/package.hpp"
 
 #include <boost/spirit/home/x3.hpp>
-#include <boost/spirit/home/x3/support/utility/annotate_on_success.hpp>
+// #include <boost/spirit/home/x3/support/utility/annotate_on_success.hpp>
 
 namespace tcc
 {
@@ -15,20 +15,21 @@ using x3::lexeme;
 using x3::raw;
 using namespace x3::ascii;
 
-auto const PackageDeclaration = PackageDeclaration_type("PackageDeclaration");
-auto const PackageDeclaration_def = PackageDeclaration;
+namespace x3 = boost::spirit::x3;
+
+
+auto const PackageDeclaration     = PackageDeclaration_type("PackageDeclaration");
+auto const PackageDeclaration_def = x3::lit("package")  //
+                                    >> identifier       //
+                                    >> x3::lit(";");    //
 BOOST_SPIRIT_DEFINE(PackageDeclaration);
 
 struct PackageDeclaration_class
-    : error_handler_base
-    , x3::annotate_on_success
+// : error_handler_base
+// : x3::annotate_on_success
 {
 };
 
 }  // namespace parser
-}  // namespace tcc
-
-namespace tcc
-{
 parser::PackageDeclaration_type const& PackageDeclaration() { return parser::PackageDeclaration; }
 }  // namespace tcc

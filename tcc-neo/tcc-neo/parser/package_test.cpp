8ed8e618a3e020baa60cbd42f8a3411e62ff77aa
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-03-31 01:24:12 +0200

    neo: Enable package declaration tests

--- tcc-neo/tcc-neo/parser/package_test.cpp
+++ tcc-neo/tcc-neo/parser/package_test.cpp
@@ -4,36 +4,36 @@
  */
 #include "catch2/catch.hpp"
 
-#include "tcc-neo/parser/package.hpp"
+#include "tcc-neo/parser/package_def.hpp"
 
-// TEST_CASE("tcc-neo/parser: PackageDeclaration", "[parser]")
-// {
-//     struct TestCase
-//     {
-//         std::string input;
-//         std::string expected;
-//     };
+TEST_CASE("tcc-neo/parser: PackageDeclaration", "[parser]")
+{
+    struct TestCase
+    {
+        std::string input;
+        std::string expected;
+    };
 
-//     auto testCases = std::vector<TestCase> {
-//         {std::string {"package main;"}, std::string {"main"}},            //
-//         {std::string {"package math;"}, std::string {"math"}},            //
-//         {std::string {"package math_test;"}, std::string {"math_test"}},  //
-//         {std::string {"package tobi_est;"}, std::string {"tobi_est"}},    //
-//         {std::string {"package fmt8;"}, std::string {"fmt8"}},            //
-//         {std::string {";"}, std::string {""}},                            //
+    auto testCases = std::vector<TestCase> {
+        {std::string {"package main;"}, std::string {"main"}},            //
+        {std::string {"package math;"}, std::string {"math"}},            //
+        {std::string {"package math_test;"}, std::string {"math_test"}},  //
+        {std::string {"package tobi_est;"}, std::string {"tobi_est"}},    //
+        {std::string {"package fmt8;"}, std::string {"fmt8"}},            //
+        {std::string {";"}, std::string {""}},                            //
 
-//     };
+    };
 
-//     for (auto const& test : testCases)
-//     {
-//         tcc::parser::packageDeclaration_t result;
-//         tcc::parser::x3::phrase_parse(std::begin(test.input),           //
-//                                       std::end(test.input),             //
-//                                       tcc::parser::packageDeclaration,  //
-//                                       tcc::parser::x3::space,           //
-//                                       result                            //
-//         );
+    for (auto const& test : testCases)
+    {
+        std::string result;
+        tcc::parser::x3::phrase_parse(std::begin(test.input),           //
+                                      std::end(test.input),             //
+                                      tcc::PackageDeclaration(),  //
+                                      tcc::parser::x3::space,           //
+                                      result                            //
+        );
 
-//         REQUIRE(result == test.expected);
-//     }
-// }
+        REQUIRE(result == test.expected);
+    }
+}

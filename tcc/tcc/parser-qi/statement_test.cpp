8e2135955329a13c800c5231aa6a0bffdb2eb546
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-04-06 19:57:17 +0200

    tcc:  Cleanup tests

--- tcc/tcc/parser-qi/statement_test.cpp
+++ tcc/tcc/parser-qi/statement_test.cpp
@@ -59,28 +59,9 @@ TEST_CASE("tcc/parser-qi: ReturnStatementValid", "[tcc][parser][qi]") {
   auto errorHandler = ErrorHandler<IteratorType>(iter, end);       // Our error handler
   auto statement = parser::Statement<IteratorType>(errorHandler);  // Our parser
   auto skipper = parser::Skipper<IteratorType>{};                  // Our skipper
-  auto ast = ast::StatementList{};                                 // Our AST
+  auto ast = ast::ReturnStatement{};                               // Our AST
 
-  bool success = phrase_parse(iter, end, statement, skipper, ast);
+  bool success = phrase_parse(iter, end, statement.ReturnStatement, skipper, ast);
 
   REQUIRE(success == true);
-}
-
-TEST_CASE("tcc/parser-qi: ReturnStatementInvalid", "[tcc][parser][qi]") {
-  using namespace client;
-
-  auto testCase = GENERATE(as<std::string>{}, "return", "return -;");
-
-  using IteratorType = std::string::const_iterator;
-  IteratorType iter = testCase.begin();
-  IteratorType end = testCase.end();
-
-  auto errorHandler = ErrorHandler<IteratorType>(iter, end);       // Our error handler
-  auto statement = parser::Statement<IteratorType>(errorHandler);  // Our parser
-  auto skipper = parser::Skipper<IteratorType>{};                  // Our skipper
-  auto ast = ast::StatementList{};                                 // Our AST
-
-  bool success = phrase_parse(iter, end, statement, skipper, ast);
-
-  REQUIRE(success == false);
 }

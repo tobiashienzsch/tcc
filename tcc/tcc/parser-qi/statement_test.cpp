76e238f208d4c0ee9d24da99e23cb305a11328c7
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-04-06 19:35:56 +0200

    tcc: Add tests for statement rules in qi parser

---
+++ tcc/tcc/parser-qi/statement_test.cpp
@@ -0,0 +1,59 @@
+/**
+ * @file statement_test.cpp
+ * @copyright Copyright 2019-2020 Tobias Hienzsch. MIT license.
+ */
+
+#include "tcc/parser-qi/statement.hpp"
+
+#include <sstream>
+
+#include "catch2/catch.hpp"
+#include "tcc/parser-qi/skipper.hpp"
+
+namespace {
+class NullBuffer : public std::streambuf {
+ public:
+  int overflow(int c) { return c; }
+};
+
+NullBuffer null_buffer;
+std::ostream null_stream(&null_buffer);
+}  // namespace
+
+TEST_CASE("tcc/parser-qi: StatementValid", "[tcc][parser][qi]") {
+  using namespace client;
+
+  auto testCase = GENERATE(as<std::string>{}, "int x = 0;", "x = y+z;");
+
+  using IteratorType = std::string::const_iterator;
+  IteratorType iter = testCase.begin();
+  IteratorType end = testCase.end();
+
+  auto errorHandler = ErrorHandler<IteratorType>(iter, end);       // Our error handler
+  auto statement = parser::Statement<IteratorType>(errorHandler);  // Our parser
+  auto skipper = parser::Skipper<IteratorType>{};                  // Our skipper
+  auto ast = ast::StatementList{};                                 // Our AST
+
+  bool success = phrase_parse(iter, end, statement, skipper, ast);
+
+  REQUIRE(success == true);
+}
+
+TEST_CASE("tcc/parser-qi: StatementInvalid", "[tcc][parser][qi]") {
+  using namespace client;
+
+  auto testCase = GENERATE(as<std::string>{}, "auto x = 0;", "= y+z;");
+
+  using IteratorType = std::string::const_iterator;
+  IteratorType iter = testCase.begin();
+  IteratorType end = testCase.end();
+
+  auto errorHandler = ErrorHandler<IteratorType>(iter, end);       // Our error handler
+  auto statement = parser::Statement<IteratorType>(errorHandler);  // Our parser
+  auto skipper = parser::Skipper<IteratorType>{};                  // Our skipper
+  auto ast = ast::StatementList{};                                 // Our AST
+
+  bool success = phrase_parse(iter, end, statement, skipper, ast);
+
+  REQUIRE(success == false);
+}

062c3e2b940ca63d4340b167e3bb51a555106d61
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-04-06 23:48:53 +0200

    tcc: Move commandline options to seperate file

--- tcc/main.cpp
+++ tcc/main.cpp
@@ -1,91 +1,25 @@
 #include <boost/lexical_cast.hpp>
-#include <boost/program_options.hpp>
-#include <fstream>
-#include <iostream>
 #include <iterator>
-#include <string>
-#include <vector>
 
 #include "tcc/asm/assembly_generator.hpp"
+#include "tcc/cmd/program_options.hpp"
 #include "tcc/ir/generator.hpp"
 #include "tcc/optimizer/optimizer.hpp"
 #include "tcc/parser/function.hpp"
 #include "tcc/parser/skipper.hpp"
 #include "tsl/tsl.hpp"
 
-namespace po = boost::program_options;
-
 int main(int argc, char** argv) {
-  std::string source = R"(
-    int a = 1 + (2 + 8 / 2) * 3;
-    int b = 1 + (2 + 8 / 2) * 3;
-    
-    int x = 1 + (2 + 8 / 2) * 3;
-    x = x * 2;
-    int y = x + 2 * 2;
-    return y * 2;
-  )";
-
-  struct CompilerFlags {
-    int OptLevel = 0;
-    bool PrintSource = false;
-    bool PrintAst = false;
-    bool PrintIR = true;
-  } flags;
-
-  try {
-    // Declare a group of options that will be
-    // allowed only on command line
-    po::options_description desc("Tobante's Crappy Compiler");
-    desc.add_options()                                                                 //
-        ("help,h", "produce this help message")                                        //
-        ("input,i", po::value<std::vector<std::string>>(), "input source file")        //
-        ("optimization,O", po::value<int>(&flags.OptLevel), "optimization level 0-1")  //
-        ("print-source", po::bool_switch(&flags.PrintSource), "print source code")     //
-        ("print-ast", po::bool_switch(&flags.PrintAst), "print parsed ast")            //
-        ("print-ir", po::bool_switch(&flags.PrintIR), "print generated ir")            //
-        ;
-
-    po::positional_options_description p;
-    p.add("input", -1);
-
-    po::variables_map vm;
-    po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm);
-    po::notify(vm);
-
-    if (vm.count("help")) {
-      std::cout << desc << "\n";
-      return 0;
-    }
-
-    if (vm.count("input")) {
-      auto const paths = vm["input"].as<std::vector<std::string>>();
-      if (paths.size() > 1) {
-        fmt::print("Only one source file allowed currently.\n");
-        return EXIT_FAILURE;
-      }
-
-      std::ifstream in(paths[0], std::ios_base::in);
-      if (!in) {
-        std::cerr << "Error: Could not open input file: " << paths[0] << std::endl;
-        return 1;
-      }
-
-      in.unsetf(std::ios::skipws);  // No white space skipping!
-      source = "";
-      std::copy(std::istream_iterator<char>(in), std::istream_iterator<char>(), std::back_inserter(source));
-    }
-
-  } catch (std::exception& e) {
-    std::cerr << "error: " << e.what() << "\n";
-    return 1;
-  } catch (...) {
-    std::cerr << "Exception of unknown type!\n";
+  auto programOptions = tcc::ProgramOptions{};
+  auto const [shouldExit, exitCode] = programOptions.ParseArguments(argc, argv);
+  auto const flags = programOptions.GetCompilerFlags();
+  if (shouldExit) {
+    return exitCode;
   }
 
   using IteratorType = std::string::const_iterator;
-  IteratorType iter = source.begin();
-  IteratorType end = source.end();
+  IteratorType iter = flags.Source.begin();
+  IteratorType end = flags.Source.end();
 
   tcc::ErrorHandler<IteratorType> errorHandler(iter, end);         // Our error handler
   tcc::parser::Statement<IteratorType> statement(errorHandler);    // Our parser
---
+++ tcc/tcc/cmd/program_options.hpp
@@ -0,0 +1,88 @@
+#if !defined(TCC_CMD_PROHRAM_OPTIONS_HPP)
+#define TCC_CMD_PROHRAM_OPTIONS_HPP
+
+#include <boost/program_options.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "tsl/tsl.hpp"
+
+namespace tcc {
+namespace po = boost::program_options;
+
+class ProgramOptions {
+ public:
+  struct CompilerFlags {
+    std::string Source = "";
+    int OptLevel = 0;
+    bool PrintSource = false;
+    bool PrintAst = false;
+    bool PrintIR = true;
+  };
+
+ public:
+  ProgramOptions() = default;
+
+  std::pair<bool, int> ParseArguments(int argc, char** argv) {
+    try {
+      // Declare a group of options that will be
+      // allowed only on command line
+      po::options_description desc("Tobante's Crappy Compiler");
+      desc.add_options()                                                                  //
+          ("help,h", "produce this help message")                                         //
+          ("input,i", po::value<std::vector<std::string>>(), "input source file")         //
+          ("optimization,O", po::value<int>(&flags_.OptLevel), "optimization level 0-1")  //
+          ("print-source", po::bool_switch(&flags_.PrintSource), "print source code")     //
+          ("print-ast", po::bool_switch(&flags_.PrintAst), "print parsed ast")            //
+          ("print-ir", po::bool_switch(&flags_.PrintIR), "print generated ir")            //
+          ;
+
+      po::positional_options_description p;
+      p.add("input", -1);
+
+      po::store(po::command_line_parser(argc, argv).options(desc).positional(p).run(), vm_);
+      po::notify(vm_);
+
+      if (vm_.count("help")) {
+        std::cout << desc << "\n";
+        return {true, EXIT_SUCCESS};
+      }
+
+      if (vm_.count("input")) {
+        auto const paths = vm_["input"].as<std::vector<std::string>>();
+        if (paths.size() > 1) {
+          fmt::print("Only one source file allowed currently.\n");
+          return {true, EXIT_FAILURE};
+        }
+
+        std::ifstream in(paths[0], std::ios_base::in);
+        if (!in) {
+          std::cerr << "Error: Could not open input file: " << paths[0] << std::endl;
+          return {true, EXIT_FAILURE};
+        }
+
+        in.unsetf(std::ios::skipws);  // No white space skipping!
+        flags_.Source = "";
+        std::copy(std::istream_iterator<char>(in), std::istream_iterator<char>(), std::back_inserter(flags_.Source));
+      }
+
+    } catch (std::exception& e) {
+      std::cerr << "error: " << e.what() << "\n";
+      return {true, EXIT_FAILURE};
+    } catch (...) {
+      std::cerr << "Exception of unknown type!\n";
+    }
+
+    return {false, EXIT_SUCCESS};
+  }
+
+  CompilerFlags const& GetCompilerFlags() const noexcept { return flags_; }
+
+ private:
+  CompilerFlags flags_{};
+  po::variables_map vm_;
+};
+}  // namespace tcc
+#endif

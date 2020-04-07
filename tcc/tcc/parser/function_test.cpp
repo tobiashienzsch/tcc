7d0fe4ceb8b7a06bbf0b492d6bff32189a02a3ad
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-04-07 02:40:14 +0200

    tcc: Add basic tests for function parse grammar

--- tcc/tcc/parser/function_test.cpp
+++ tcc/tcc/parser/function_test.cpp
@@ -13,16 +13,16 @@
 TEST_CASE("tcc/parser: FunctionValid", "[tcc][parser][qi]") {
   using namespace tcc;
 
-  auto testCase = GENERATE(as<std::string>{}, "int x = 0;", "x = y+z;");
+  auto testCase = GENERATE(as<std::string>{}, "void foo(){ int foo = 5; }", "int foo(){return 1;}");
 
   using IteratorType = std::string::const_iterator;
   IteratorType iter = testCase.begin();
   IteratorType end = testCase.end();
 
-  auto errorHandler = ErrorHandler<IteratorType>(iter, end);       // Our error handler
+  auto errorHandler = ErrorHandler<IteratorType>(iter, end);     // Our error handler
   auto function = parser::Function<IteratorType>(errorHandler);  // Our parser
-  auto skipper = parser::Skipper<IteratorType>{};                  // Our skipper
-  auto ast = ast::FunctionList{};                                 // Our AST
+  auto skipper = parser::Skipper<IteratorType>{};                // Our skipper
+  auto ast = ast::FunctionList{};                                // Our AST
 
   bool success = phrase_parse(iter, end, function, skipper, ast);
 
@@ -32,16 +32,16 @@ TEST_CASE("tcc/parser: FunctionValid", "[tcc][parser][qi]") {
 TEST_CASE("tcc/parser: FunctionInvalid", "[tcc][parser][qi]") {
   using namespace tcc;
 
-  auto testCase = GENERATE(as<std::string>{}, "auto x = 0;", "= y+z;");
+  auto testCase = GENERATE(as<std::string>{}, "int int(){}", "void void(){}");
 
   using IteratorType = std::string::const_iterator;
   IteratorType iter = testCase.begin();
   IteratorType end = testCase.end();
 
-  auto errorHandler = ErrorHandler<IteratorType>(iter, end);       // Our error handler
+  auto errorHandler = ErrorHandler<IteratorType>(iter, end);     // Our error handler
   auto function = parser::Function<IteratorType>(errorHandler);  // Our parser
-  auto skipper = parser::Skipper<IteratorType>{};                  // Our skipper
-  auto ast = ast::FunctionList{};                                 // Our AST
+  auto skipper = parser::Skipper<IteratorType>{};                // Our skipper
+  auto ast = ast::FunctionList{};                                // Our AST
 
   bool success = phrase_parse(iter, end, function, skipper, ast);
 

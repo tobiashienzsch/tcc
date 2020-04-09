08b05f23599a9235f2aebceff20a01f1cbbfd942
Tobias Hienzsch <post@tobias-hienzsch.de>
2020-04-09 17:28:10 +0200

    Add more integration tests

--- tests/integration_test.cpp
+++ tests/integration_test.cpp
@@ -27,16 +27,17 @@ TEST_CASE("integration: CompileAndRunConstant", "[integration]")
         auto const stackSize    = 200;
         auto const entryPoint   = 0;
         auto const globalMemory = 0;
+        auto const assembly     = compiler.GetAssembly();
 
-        auto vm = tcc::VirtualMachine(compiler.GetAssembly(), entryPoint, globalMemory, stackSize, false);
+        auto vm = tcc::VirtualMachine(assembly, entryPoint, globalMemory, stackSize, false);
         REQUIRE(vm.Cpu() == 1);
     }
 }
 
-TEST_CASE("integration: CompileAndRunExpression", "[integration]")
+TEST_CASE("integration: CompileAndRunAddition", "[integration]")
 {
     auto source = std::string {R"(
-        int main() { return (1+2+3+4)*2;}
+        int main() { return 1+2+3+4;}
     )"};
 
     for (auto optLevel : {0, 1})
@@ -51,13 +52,85 @@ TEST_CASE("integration: CompileAndRunExpression", "[integration]")
         auto const stackSize    = 200;
         auto const entryPoint   = 0;
         auto const globalMemory = 0;
-
-        auto vm = tcc::VirtualMachine(compiler.GetAssembly(), entryPoint, globalMemory, stackSize, false);
-        REQUIRE(vm.Cpu() == 20);
+        auto const assembly     = compiler.GetAssembly();
+        auto vm                 = tcc::VirtualMachine(assembly, entryPoint, globalMemory, stackSize, false);
+        REQUIRE(vm.Cpu() == 10);
     }
 }
 
-TEST_CASE("integration: CompileAndRunVariables", "[integration]")
+TEST_CASE("integration: CompileAndRunSubtraction", "[integration]")
+{
+    auto source = std::string {R"(
+        int main() { return 30-20-9;}
+    )"};
+
+    for (auto optLevel : {0, 1})
+    {
+        auto options     = tcc::CompilerOptions {};
+        options.Source   = source;
+        options.OptLevel = optLevel;
+        auto compiler    = tcc::Compiler {options};
+
+        REQUIRE(compiler.Run() == EXIT_SUCCESS);
+
+        auto const stackSize    = 200;
+        auto const entryPoint   = 0;
+        auto const globalMemory = 0;
+        auto const assembly     = compiler.GetAssembly();
+        auto vm                 = tcc::VirtualMachine(assembly, entryPoint, globalMemory, stackSize, false);
+        REQUIRE(vm.Cpu() == 1);
+    }
+}
+
+TEST_CASE("integration: CompileAndRunMultiplication", "[integration]")
+{
+    auto source = std::string {R"(
+        int main() { return 10*10*5;}
+    )"};
+
+    for (auto optLevel : {0, 1})
+    {
+        auto options     = tcc::CompilerOptions {};
+        options.Source   = source;
+        options.OptLevel = optLevel;
+        auto compiler    = tcc::Compiler {options};
+
+        REQUIRE(compiler.Run() == EXIT_SUCCESS);
+
+        auto const stackSize    = 200;
+        auto const entryPoint   = 0;
+        auto const globalMemory = 0;
+        auto const assembly     = compiler.GetAssembly();
+        auto vm                 = tcc::VirtualMachine(assembly, entryPoint, globalMemory, stackSize, false);
+        REQUIRE(vm.Cpu() == 500);
+    }
+}
+
+TEST_CASE("integration: CompileAndRunMixedExpression", "[integration]")
+{
+    auto source = std::string {R"(
+        int main() { return 10*10*5+10-20;}
+    )"};
+
+    for (auto optLevel : {0, 1})
+    {
+        auto options     = tcc::CompilerOptions {};
+        options.Source   = source;
+        options.OptLevel = optLevel;
+        auto compiler    = tcc::Compiler {options};
+
+        REQUIRE(compiler.Run() == EXIT_SUCCESS);
+
+        auto const stackSize    = 200;
+        auto const entryPoint   = 0;
+        auto const globalMemory = 0;
+        auto const assembly     = compiler.GetAssembly();
+        auto vm                 = tcc::VirtualMachine(assembly, entryPoint, globalMemory, stackSize, false);
+        REQUIRE(vm.Cpu() == 490);
+    }
+}
+
+TEST_CASE("integration: CompileAndRunLocalVars", "[integration]")
 {
     auto source = std::string {R"(
         int main() { 
@@ -78,8 +151,9 @@ TEST_CASE("integration: CompileAndRunVariables", "[integration]")
         auto const stackSize    = 200;
         auto const entryPoint   = 0;
         auto const globalMemory = 0;
+        auto const assembly     = compiler.GetAssembly();
 
-        auto vm = tcc::VirtualMachine(compiler.GetAssembly(), entryPoint, globalMemory, stackSize, false);
+        auto vm = tcc::VirtualMachine(assembly, entryPoint, globalMemory, stackSize, false);
         REQUIRE(vm.Cpu() == 60);
     }
 }
